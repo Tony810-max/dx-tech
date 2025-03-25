2fc2fffcf90c5390257a7928d7eb5947cdf76380
Tony810-mã <quangvu37781@gmail.com>
2025-03-25 16:23:03 +0700

    feat: initial project

---
+++ src/modules/LandingPage/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="font-sans  container mx-auto">
+      LandingPage
+      <Button variant={"destructive"}>click me</Button>
+    </div>
+  );
+};
+
+export default LandingPage;
