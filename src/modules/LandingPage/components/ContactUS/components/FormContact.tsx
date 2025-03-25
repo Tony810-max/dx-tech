f2609799e66ee032a37ee16050cd03ffcdf3ac5d
Tony810-mã <quangvu37781@gmail.com>
2025-03-26 03:23:14 +0700

    feat: validate form

--- src/modules/LandingPage/components/ContactUS/components/FormContact.tsx
+++ src/modules/LandingPage/components/ContactUS/components/FormContact.tsx
@@ -1,12 +1,35 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object()
+  .shape({
+    email: yup.string().email().required(),
+    message: yup.string().required(),
+  })
+  .required();
 
 const FormContact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
   return (
-    <form className="absolute pt-[3.75rem] pl-[6.25rem] pb-20 min-w-[34.75rem] space-y-10">
+    <form
+      className="absolute pt-[3.75rem] pl-[6.25rem] pb-20 min-w-[34.75rem] space-y-10"
+      onSubmit={handleSubmit((d) => console.log(d))}
+    >
       <RadioGroup className="flex items-center gap-[2.1875rem]">
         <div className="flex items-center gap-[.875rem]">
           <RadioGroupItem
@@ -42,14 +65,25 @@ const FormContact = () => {
           <Input
             className="py-[1.125rem] pl-[30px] border-black bg-white"
             placeholder="Email"
+            {...register("email")}
           />
+          {errors.email?.message && (
+            <p className="text-red-500 italic">{errors.email?.message}</p>
+          )}
         </div>
         <div className="space-y-[.3125rem]">
-          <label>Message**</label>
-          <Textarea placeholder="Message" className="resize-none bg-white" />
+          <label>Message*</label>
+          <Textarea
+            placeholder="Message"
+            className="resize-none bg-white"
+            {...register("message")}
+          />
+          {errors.message?.message && (
+            <p className="text-red-500 italic">{errors.message?.message}</p>
+          )}
         </div>
       </div>
-      <Button variant={"secondary"} className="w-full">
+      <Button type="submit" variant={"secondary"} className="w-full">
         Send Message
       </Button>
     </form>
